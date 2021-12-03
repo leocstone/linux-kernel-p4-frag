7a3fa2536a7ca96d3292cb3ac90d4fecda64d19d
gregbolet <gbolet@vt.edu>
2021-12-03 14:18:06 -0500

    added fragmentation code inputs

--- forced-frag/forceFrag.c
+++ forced-frag/forceFrag.c
@@ -6,15 +6,29 @@
 
 int main(int argc, char** argv){
 
-	printf("Forcing Fragmentation!\n");
 
 	char *p;
 	size_t i;
+	int numpages = 10000000;
+
+	if(argc != 2){
+		fprintf(stderr, "Not enough arguments! Provided %d, need 1\n", argc-1);
+		return -1;
+	}
+
+	printf("Forcing Fragmentation!\n");
+
+	if (sscanf (argv[1], "%i", &numpages) != 1) {
+    		fprintf(stderr, "error - not an integer\n");
+		return -1;
+	}
+
+	printf("Creating %d pages\n", numpages);
 
 	// Get the end position of this process's data segment
 	p = sbrk(0);
 
-	for(i = 0; i < 10000000; i++){
+	for(i = 0; i < numpages; i++){
 		// Extend the process data segment by 1 page
 		// Though this appears contiguous in virtual memory
 		// it may not be at the physical memory level
