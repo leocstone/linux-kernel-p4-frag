5f7bcc7b0b89985fddc543bc3b74a91b4af68be1
gregbolet <gbolet@vt.edu>
2021-12-04 10:25:07 -0500

    added sweep of compact threshold

--- test-frag.sh
+++ test-frag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The number of trials to perform
-NUM_TRIALS=10
+NUM_TRIALS=5
 
 # The order of the number of pages to allocate 
 # Example: if FRAG_ORDER_TO_ALLOC=10, 2^10 = 1024 pages
@@ -14,41 +14,46 @@ NUM_TRIALS=10
 # 18 --> (2^18)*4096 = 1  GiB
 FRAG_ORDER_TO_ALLOC=22
 
-SAMPLE_RATE_SECS=3
+SAMPLE_RATE_SECS=1
 COMPACT_ORDER=4
-COMPACT_THRESH=20
+COMPACT_THRESH_MIN=20
+COMPACT_THRESH_MAX=80
 
-for ((TRIAL=1;TRIAL<=$NUM_TRIALS;TRIAL+=1)); do
+# Let's sweep over the threshold parameter
+for ((TRIAL_THRESH=$COMPACT_THRESH_MIN;TRIAL_THRESH<=$COMPACT_THRESH_MAX;TRIAL_THRESH+=10)); do
 
-	sudo rmmod frag.ko
-	sudo insmod frag.ko rate=$SAMPLE_RATE_SECS \
-			    compaction_order=$COMPACT_ORDER \
-			    compaction_thresh=$COMPACT_THRESH
+	for ((TRIAL=1;TRIAL<=$NUM_TRIALS;TRIAL+=1)); do
 	
-	# Start recording and automatic compaction
-	cat /proc/frag/record
+		sudo rmmod frag.ko
+		sudo insmod frag.ko rate=$SAMPLE_RATE_SECS \
+				    compaction_order=$COMPACT_ORDER \
+				    compaction_thresh=$TRIAL_THRESH
+		
+		# Start recording and automatic compaction
+		cat /proc/frag/record
+		
+		# Now lets launch the pgbench job to introduce more contention
+		# Need to login to this postgres account and lauch it
+		sudo runuser -l postgres -c 'pgbench -c 10 -j 6 -t 50000 example &'
+		
+		# Wait for the processes to start up
+		sleep 5
+		
+		# Run the forced fragmentation codes, it will make
+		# it so that the physical memory is forced to interleave the
+		# allocatin of pages for each process, in-turn fragmenting the
+		# the memory.
+		./forced-frag/forceFrag $FRAG_ORDER_TO_ALLOC & \
+		./forced-frag/forceFrag $FRAG_ORDER_TO_ALLOC
+		
+		# Wait for there 6 runs to finish...
+		
+		cat /proc/frag/last_recording >> ./rundata/order_${FRAG_ORDER_TO_ALLOC}_comporder_${COMPACT_ORDER}_compthresh_${TRIAL_THRESH}_rate_${SAMPLE_RATE_SECS}_trial_${TRIAL}_rundata.csv
+		sudo rmmod frag.ko
+		
+		# Kill pgbench if it's still running
+		sudo killall pgbench
 	
-	# Now lets launch the pgbench job to introduce more contention
-	# Need to login to this postgres account and lauch it
-	sudo runuser -l postgres -c 'pgbench -c 10 -j 6 -t 50000 example &'
-	
-	# Wait for the processes to start up
-	sleep 5
-	
-	# Run the forced fragmentation codes, it will make
-	# it so that the physical memory is forced to interleave the
-	# allocatin of pages for each process, in-turn fragmenting the
-	# the memory.
-	./forced-frag/forceFrag $FRAG_ORDER_TO_ALLOC & \
-	./forced-frag/forceFrag $FRAG_ORDER_TO_ALLOC
-	
-	# Wait for there 6 runs to finish...
-	
-	cat /proc/frag/last_recording >> ./rundata/order_${FRAG_ORDER_TO_ALLOC}_comporder_${COMPACT_ORDER}_compthresh_${COMPACT_THRESH}_rate_${SAMPLE_RATE_SECS}_trial_${TRIAL}_rundata.csv
-	sudo rmmod frag.ko
-	
-	# Kill pgbench if it's still running
-	sudo killall pgbench
-
+	done
 done
 
