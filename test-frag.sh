24aa3c45ac4db28a8ac58eaf2bf197e1c2143087
gregbolet <gbolet@vt.edu>
2021-12-03 19:01:01 -0500

    working on job launch script

--- test-frag.sh
+++ test-frag.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+sudo rmmod frag.ko
+sudo insmod frag.ko
+
+# Start recording and automatic compaction
+cat /proc/frag/record
+
+# Now lets launch the pgbench job to introduce more contention
+# Need to login to this postgres account and lauch it
+sudo su - postgres
+nohup pgbench -c 10 -j 6 -t 50000 example & 
+exit
+
+echo "out of pgbench launch"
+
 # The order of the number of pages to allocate 
 # Example: if ORDER_TO_ALLOC=10, 2^10 = 1024 pages
 # will be allocated by each fragmentation code
-ORDER_TO_ALLOC=21
+ORDER_TO_ALLOC=20
 
 # Run all six at the same time to force a little fragmentation
 # These six correspond to the number of cores on our test cpu
@@ -13,7 +27,11 @@ ORDER_TO_ALLOC=21
 ./forced-frag/forceFrag $ORDER_TO_ALLOC & \
 ./forced-frag/forceFrag $ORDER_TO_ALLOC & \
 ./forced-frag/forceFrag $ORDER_TO_ALLOC & \
-./forced-frag/forceFrag $ORDER_TO_ALLOC
+./forced-frag/forceFrag $ORDER_TO_ALLOC & \
+./forced-frag/forceFrag $ORDER_TO_ALLOC & \
+./forced-frag/forceFrag $ORDER_TO_ALLOC 
+
+# Wait for there 6 runs to finish...
 
 # Now let's run the module and see what happens 
 # before and after manual compaction
@@ -21,7 +39,10 @@ ORDER_TO_ALLOC=21
 #cat /proc/frag/info
 #sudo rmmod frag.ko
 
-dmesg | tail -n 15
+#dmesg | tail -n 15
+
+cat /proc/frag/last_recording >> order_${ORDER_TO_ALLOC}_rundata.csv
+sudo rmmod frag.ko
 
 #cat /proc/frag/record
 
